91948e937eda88b47879262c391acf4d8cc56e5c
Edwin Martinez <edwinmartinezgav@gmail.com>
2024-11-14 18:19:03 -0500

    mejoras en el registro

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function abrir(){
     document.getElementById("loginW").style.display="block";
     document.getElementById("backgroundOverlay").style.display="block";
 }
+
+function confirmacion(){
+    document.getElementById("registro-confirmacion").style.display="block";
+}
